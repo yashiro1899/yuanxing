8a709985e068423d57d8398a01a677ed02848b7f
Ice White <yashiro1899@gmail.com>
2014-03-10 19:40:04 +0800

    1394451604

--- getallhotels.js
+++ getallhotels.js
@@ -3,14 +3,13 @@ var Promise = require('es6-promise').Promise;
 
 var data = [];
 var i = 0, j, hotelIds;
-for (; i < 510; i += 1) {
+for (; i < 5; i += 1) {
     hotelIds = [];
     for (j = (i * 20 + 1); j <= ((i + 1) * 20); j += 1) hotelIds.push(j);
     hotelIds = hotelIds.join("/")
     data.push(hotelIds);
 }
 
-console.log(+(new Date()));
 data.reduce(function(sequence, ids) {
     return jielvapi({
         "QueryType": "hotelinfo",
@@ -18,7 +17,23 @@ data.reduce(function(sequence, ids) {
     }).then(function(result) {
         if (!result) return false;
 
-        console.log(ids + " ----------> " + result.data.length);
-        console.log(+(new Date()));
+        result.data.forEach(function(hotel) {
+            if (hotel.namechn == "香港万怡酒店") {
+                var roomtypeids = [];
+                hotel.rooms.forEach(function(room) {
+                    roomtypeids.push(room.roomtypeid);
+                });
+
+                jielvapi({
+                    "QueryType": "hotelpriceall",
+                    "roomtypeids": roomtypeids.join("/"),
+                    "checkInDate": "2014-03-11",
+                    "checkOutDate": "2014-03-14"
+                }).then(function(price) {
+                    price = JSON.stringify(price);
+                    console.log(price)
+                });
+            }
+        });
     });
 }, Promise.resolve());
