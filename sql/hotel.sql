9673debfcc20644ac2504379b3780123c940b6c9
Ice White <yashiro1899@gmail.com>
2014-03-11 15:23:36 +0800

    1394522616

--- sql/hotel.sql
+++ sql/hotel.sql
@@ -1,14 +1,15 @@
 CREATE TABLE IF NOT EXISTS `think_hotel` (
-    `hotelid` INT(11) UNSIGNED NOT NULL,
-    `hotelcd` VARCHAR(32) NOT NULL,
-    `namechn` VARCHAR(64) NOT NULL,
-    `nameeng` VARCHAR(64) NOT NULL,
-    `country` INT(11) UNSIGNED NOT NULL,
-    `state`   INT(11) UNSIGNED NOT NULL,
-    `city`    INT(11) UNSIGNED NOT NULL,
-    `website` VARCHAR(128) NOT NULL,
+    `hotelid`    INT(11) UNSIGNED NOT NULL,
+    `hotelcd`    VARCHAR(32) NOT NULL,
+    `namechn`    VARCHAR(64) NOT NULL,
+    `nameeng`    VARCHAR(64) NOT NULL,
+    `country`    INT(11) UNSIGNED NOT NULL,
+    `state`      INT(11) UNSIGNED NOT NULL,
+    `city`       INT(11) UNSIGNED NOT NULL,
+    `website`    VARCHAR(128) NOT NULL,
+    `taobao_hid` INT(11) UNSIGNED DEFAULT 0,
     PRIMARY KEY (`hotelid`),
     KEY country (`country`),
     KEY state (`state`),
     KEY city (`city`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
