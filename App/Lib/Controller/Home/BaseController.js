728989b18b04548294e39a792f2059d722f9e4c9
Ice White <yashiro1899@gmail.com>
2014-03-13 14:28:10 +0800

    1394692090

--- App/Lib/Controller/Home/BaseController.js
+++ App/Lib/Controller/Home/BaseController.js
@@ -16,6 +16,46 @@ module.exports = Controller(function() {
                 that.userInfo = u;
                 that.assign("userInfo", that.userInfo);
             });
+        },
+        pagination: function(total, range, current) {
+            var ON_EACH_SIDE = 3;
+            var ON_ENDS = 2;
+            var pagination = {
+                required: false
+            };
+            var list, num_pages, i;
+
+            if (total > 0 && range > 0 && total > range) {
+                num_pages = Math.ceil(total / 20);
+                pagination["required"] = true;
+                pagination["total"] = total;
+                pagination["range"] = range;
+                pagination["num_pages"] = num_pages;
+                pagination["item"] = "Hotels";
+
+                list = [];
+                if (num_pages <= 8) {
+                    for (i = 1; i <= num_pages; i += 1) list.push(i);
+                } else {
+                    if (current > (ON_EACH_SIDE + ON_ENDS + 1)) {
+                        for (i = 1; i <= ON_ENDS; i += 1) list.push(i);
+                        list.push(".");
+                        for (i = current - ON_EACH_SIDE; i <= current; i += 1) list.push(i);
+                    } else {
+                        for (i = 1; i <= current; i += 1) list.push(i);
+                    }
+
+                    if (current < (num_pages - ON_EACH_SIDE - ON_ENDS)) {
+                        for (i = current + 1; i <= current + ON_EACH_SIDE; i += 1) list.push(i);
+                        list.push(".");
+                        for (i = num_pages - ON_ENDS; i <= num_pages; i += 1) list.push(i);
+                    } else {
+                        for (i = current + 1; i <= num_pages; i += 1) list.push(i);
+                    }
+                }
+                pagination["list"] = list;
+            }
+            return pagination;
         }
     };
 });
--- App/Lib/Controller/Home/PublishController.js
+++ App/Lib/Controller/Home/PublishController.js
@@ -14,11 +14,7 @@ module.exports = Controller("Home/BaseController", function() {
             var res = this.http.res;
 
             var page = parseInt(this.param("p"), 10) || 1;
-            var pagination = {
-                required: false,
-                current: page,
-                item: "Hotels"
-            };
+            var range = 0;
             var promise = D("Hotel").field("hotelid").order("hotelid").page(page).select();
 
             promise = promise.then(function(result) {
@@ -33,7 +29,7 @@ module.exports = Controller("Home/BaseController", function() {
                 var data = [];
                 if (result && result.success == 1) data = result.data;
 
-                pagination["range"] = data.length;
+                range = data.length;
                 data = data.map(function(h) {
                     var website = h.website.trim();
                     if (website.length > 0 && !(/^http/.test(website))) website = "http://" + website;
@@ -44,39 +40,8 @@ module.exports = Controller("Home/BaseController", function() {
                 return D("Hotel").count();
             }).then(function(result) {
                 var total = result || 0;
-                var range = pagination.range;
-                var current = pagination.current;
-                var list = [], num_pages, i;
-                var ON_EACH_SIDE = 3;
-                var ON_ENDS = 2;
+                var pagination = that.pagination(total, range, page);
 
-                if (total > 0 && range > 0 && total > range) {
-                    num_pages = Math.ceil(result / 20);
-                    pagination["required"] = true;
-                    pagination["total"] = result;
-                    pagination["num_pages"] = num_pages;
-
-                    if (num_pages <= 8) {
-                        for (i = 1; i <= num_pages; i += 1) list.push(i);
-                    } else {
-                        if (current > (ON_EACH_SIDE + ON_ENDS + 1)) {
-                            for (i = 1; i <= ON_ENDS; i += 1) list.push(i);
-                            list.push(".");
-                            for (i = current - ON_EACH_SIDE; i <= current; i += 1) list.push(i);
-                        } else {
-                            for (i = 1; i <= current; i += 1) list.push(i);
-                        }
-
-                        if (current < (num_pages - ON_EACH_SIDE - ON_ENDS)) {
-                            for (i = current + 1; i <= current + ON_EACH_SIDE; i += 1) list.push(i);
-                            list.push(".");
-                            for (i = num_pages - ON_ENDS; i <= num_pages; i += 1) list.push(i);
-                        } else {
-                            for (i = current + 1; i <= num_pages; i += 1) list.push(i);
-                        }
-                    }
-                    pagination["list"] = list;
-                }
                 that.assign('pagination', pagination);
                 that.display();
             });
