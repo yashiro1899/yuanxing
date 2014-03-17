61c516993f9a74e2428d9b68913e804ac9f6fd77
Ice White <yashiro1899@gmail.com>
2014-03-17 11:29:49 +0800

    1395026989

--- App/Lib/Controller/Home/PublishController.js
+++ App/Lib/Controller/Home/PublishController.js
@@ -16,39 +16,50 @@ module.exports = Controller("Home/BaseController", function() {
 
             var page = parseInt(this.param("p"), 10) || 1;
             var range = 0;
-            var query = this.param("q");
+            var query = this.param("q").trim();
+            var city = this.param("c").trim();
             var formdata = {};
             var model = D("Hotel");
             if (query.length > 0) {
                 formdata["q"] = query;
                 model = model.where("namechn like '%" + query + "%'");
             }
+            if (city.length > 0) {
+                formdata["c"] = city;
+                model = model.where({city: city});
+            }
+            this.assign("hotcities", this.hotcities);
             this.assign("formdata", formdata);
 
-            var promise = model.field("hotelid").order("hotelid").page(page).select();
+            var promise = model.order("hotelid").page(page).select();
             promise = promise.then(function(result) {
                 var ids = result.map(function(h) {
                     return h.hotelid;
                 });
-                return jielvapi({
-                    "QueryType": "hotelinfo",
-                    "hotelIds": ids.join("/")
+                data = result;
+                data.forEach(function(h) {
+                    h.rooms = [];
                 });
-            }).then(function(result) {
-                var data = [];
-                if (result && result.success == 1) data = result.data;
+                // return jielvapi({
+                //     "QueryType": "hotelinfo",
+                //     "hotelIds": ids.join("/")
+                // });
+            // }).then(function(result) {
+                // var data = [];
+                // if (result && result.success == 1) data = result.data;
 
                 range = data.length;
-                data = data.map(function(h) {
-                    var website = h.website.trim();
-                    if (website.length > 0 && !(/^http/.test(website))) website = "http://" + website;
-                    h.website = website;
-                    return h;
-                });
+                // data = data.map(function(h) {
+                //     var website = h.website.trim();
+                //     if (website.length > 0 && !(/^http/.test(website))) website = "http://" + website;
+                //     h.website = website;
+                //     return h;
+                // });
                 that.assign("list", data);
 
                 model = D("Hotel");
                 if (query.length > 0) model = model.where("namechn like '%" + query + "%'");
+                if (city.length > 0) model = model.where({city: city});
                 return model.count();
             }).then(function(result) {
                 var total = result || 0;
