e1a974950be8c460341131adc5cc1ae756b8d006
Ice White <yashiro1899@gmail.com>
2014-03-18 09:23:08 +0800

    1395105788

--- sql/room.sql
+++ sql/room.sql
@@ -4,7 +4,10 @@ CREATE TABLE IF NOT EXISTS `think_room` (
     `taobao_hid` INT(11) UNSIGNED NOT NULL,
     `taobao_rid` INT(11) UNSIGNED NOT NULL,
     `namechn`    VARCHAR(64) NOT NULL,
+    `status`     INT(11) UNSIGNED NOT NULL,
     `updated_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     PRIMARY KEY (`roomtypeid`),
+    KEY taobao_hid (`taobao_hid`),
+    KEY taobao_rid (`taobao_rid`),
     CONSTRAINT `hotelid` FOREIGN KEY (`hotelid`) REFERENCES `think_hotel` (`hotelid`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
