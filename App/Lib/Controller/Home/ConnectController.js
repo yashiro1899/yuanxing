dbfbd5bf0b4170b4d3738b99d7dfd83b9ca95726
Ice White <yashiro1899@gmail.com>
2014-04-03 03:26:56 +0800

    1396466816

--- App/Lib/Controller/Home/ConnectController.js
+++ App/Lib/Controller/Home/ConnectController.js
@@ -156,6 +156,7 @@ module.exports = Controller("Home/BaseController", function() {
             this.assign("formdata", formdata);
             this.assign("tab", "inventory");
 
+            var goods = [];
             var promise = oauth.accessProtectedResource(req, res, params);
             promise = promise.then(function(result) {
                 if (result && result["items_inventory_get_response"]) {
@@ -182,13 +183,73 @@ module.exports = Controller("Home/BaseController", function() {
                 } else {
                     result = [];
                 }
+                goods = result;
                 range = result.length;
 
                 var qs = querystring.stringify(formdata);
                 var pagination = that.pagination(total, range, page, qs, 101);
-
-                that.assign("list", result);
                 that.assign('pagination', pagination);
+
+                var ids = goods.map(function(g, i) {
+                    goods[i]["goodstatus"] = 0;
+                    goods[i]["goodstatusicon"] = mapping.goodstatus[0];
+                    return g.gid;
+                });
+                if (ids.length === 0) {
+                    that.assign("list", goods);
+                    that.display();
+                    return getDefer().promise;
+                }
+                return D("Goods").field("gid,status").where("gid in (" + ids.join(",") + ")").select();
+            }).then(function(result) {
+                var exists = {};
+                result = result || [];
+                result.forEach(function(g) {exists[g.gid] = g.status;});
+                goods.forEach(function(g, i) {
+                    if (exists[g.gid] && exists[g.gid] == 4) {
+                        goods[i]["goodstatus"] = 2;
+                        goods[i]["goodstatusicon"] = mapping.goodstatus[2];
+                    }
+                });
+
+                var ids = goods.filter(function(g) {if (g.goodstatus === 0) return true;});
+                ids = ids.map(function(g) {return g.hid;});
+                if (ids.length === 0) {
+                    that.assign("list", goods);
+                    that.display();
+                    return getDefer().promise;
+                }
+                return D("Hotel").field("taobao_hid").where("taobao_hid in (" + ids.join(",") + ")").select();
+            }).then(function(result) {
+                var exists = {};
+                result = result || [];
+                result.forEach(function(h) {exists[h.taobao_hid] = true;});
+                goods.forEach(function(g, i) {
+                    if (exists[g.hid]) {
+                        goods[i]["goodstatus"] = 1;
+                        goods[i]["goodstatusicon"] = mapping.goodstatus[1];
+                    }
+                });
+
+                var ids = goods.filter(function(g) {if (g.goodstatus === 1) return true;});
+                ids = ids.map(function(g) {return g.rid;});
+                if (ids.length === 0) {
+                    that.assign("list", goods);
+                    that.display();
+                    return getDefer().promise;
+                }
+                return D("Room").field("taobao_rid").where("taobao_rid in (" + ids.join(",") + ")").select();
+            }).then(function(result) {
+                var exists = {};
+                result = result || [];
+                result.forEach(function(r) {exists[r.taobao_rid] = true;});
+                goods.forEach(function(g, i) {
+                    if (exists[g.rid]) {
+                        goods[i]["goodstatus"] = 128;
+                        goods[i]["goodstatusicon"] = "<img src=\"/static/img/icon-yes.gif\" />";
+                    }
+                });
+                that.assign("list", goods);
                 that.display("connect:index");
             });
 
