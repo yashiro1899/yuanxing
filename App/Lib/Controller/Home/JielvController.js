fd532062d5a7a283badaf9baaecb70b8046a2575
Ice White <yashiro1899@gmail.com>
2014-04-08 18:26:17 +0800

    1396952777

---
+++ App/Lib/Controller/Home/JielvController.js
@@ -0,0 +1,50 @@
+/**
+ * controller
+ * @return
+ */
+var cookie = require("cookie");
+var oauth = require("../../../../taobao-oauth");
+var querystring = require('querystring');
+function rot13(s) {
+    var i;
+    var rotated = '';
+    s = s || "";
+    for (i = 0; i < s.length; i++) {
+        var ch = s.charCodeAt(i);
+        // a-z -> n-m
+        if (97 <= ch && ch <= 122) {
+            rotated += String.fromCharCode((ch - 97 + 13) % 26 + 97);
+            // A-Z -> N-M
+        } else if (65 <= ch && ch <= 90) {
+            rotated += String.fromCharCode((ch - 65 + 13) % 26 + 65);
+        } else {
+            rotated += s[i];
+        }
+    }
+    return rotated;
+}
+module.exports = Controller(function() {
+    return {
+        callbackAction: function() {
+            var that = this;
+            var req = this.http.req;
+            var res = this.http.res;
+
+            var now = +(new Date());
+            var data = {
+                "taobao_user_id": 0,
+                "taobao_user_nick": "",
+                "access_token": ""
+            };
+            data = querystring.stringify(data);
+            data = rot13(data);
+            data = cookie.serialize("access_token.taobao", data, {
+                path: "/",
+                expires: (new Date(24 * 60 * 60 * 1000 + now))
+            });
+
+            res.setHeader("Set-Cookie", data);
+            this.end("haha");
+        }
+    };
+});
