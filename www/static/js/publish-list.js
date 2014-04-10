8edef3ca6c962b7382440ca248d36ca9f5eb0ad4
Ice White <yashiro1899@gmail.com>
2014-04-11 02:02:46 +0800

    1397152966

--- www/static/js/publish-list.js
+++ www/static/js/publish-list.js
@@ -76,4 +76,49 @@ $(function() {
         elActionSelect.prop('checked', checked);
         elActionCounter.html((checked ? total : 0) + " of " + total + " selected");
     });
+
+    $("#action-go").click(function(e) {
+        var selected = [];
+        elActionSelect.each(function(i, el) {
+            if ($(el).prop("checked")) selected.push(el);
+        });
+        if (selected.length === 0) return false;
+
+        var action = $(".actions [name=action]").val();
+        switch (action) {
+            case "publish_selected":
+                if (!window.confirm("确认发布所选 " + selected.length + " 项？")) return false;
+                selected.reduce(function(sequence, el) {
+                    return sequence.then(function(result) {
+                        var td = $(el).parent().next();
+                        td.html("询价中…");
+                        return $.ajax("/publish/inquiry/", {
+                            type: "post",
+                            dataType: "json",
+                            data: "roomtypeid=" + $(el).val()
+                        }).then(function(result) {
+                            if (result["success"] == 8) {
+                                td.html("");
+                                td.prev().html(NOPRICE_ICON);
+                                return null;
+                            }
+
+                            var data = JSON.stringify(result);
+                            td.html("发布中…");
+                            return $.ajax("/publish/create/", {
+                                type: "post",
+                                dataType: "json",
+                                data: "data=" + data
+                            });
+                        }).then(function(result) {
+                            if (result && result["success"] == 1) td.html("发布成功");
+                            else if (result) td.prev().html(NOPRICE_ICON);
+                        });
+                    });
+                }, $().promise());
+                break;
+            default:
+                break;
+        }
+    });
 });
