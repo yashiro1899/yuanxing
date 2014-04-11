cec9322b6c1436741515a98267ff92fbce6ee04d
Ice White <yashiro1899@gmail.com>
2014-04-11 14:56:17 +0800

    1397199377

--- sql/room.sql
+++ sql/room.sql
@@ -5,7 +5,7 @@ CREATE TABLE IF NOT EXISTS `think_room` (
     `status`           INT(11) UNSIGNED NOT NULL DEFAULT 0,
     `original`         TEXT NOT NULL,
     `taobao_rid`       INT(11) UNSIGNED NOT NULL DEFAULT 0,
-    `no_price_expires` BIGINT(12) UNSIGNED NOT NULL DEFAULT 0,
+    `no_price_expires` BIGINT(13) UNSIGNED NOT NULL DEFAULT 0,
     `updated_at`       TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     PRIMARY KEY (`roomtypeid`),
     KEY taobao_rid (`taobao_rid`),
