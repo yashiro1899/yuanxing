c9d130554e8970bdf6762a6e47a9513fc58aaeb0
Ice White <yashiro1899@gmail.com>
2014-04-13 20:36:42 +0800

    1397392602

--- sql/user.sql
+++ sql/user.sql
@@ -2,6 +2,9 @@ CREATE TABLE IF NOT EXISTS `think_user` (
     `id`         INT(11) UNSIGNED NOT NULL,
     `nick`       VARCHAR(64) NOT NULL,
     `token`      VARCHAR(64) NOT NULL,
+    `expires`    BIGINT(13) UNSIGNED NOT NULL,
+    `pic_path`   VARCHAR(128) NOT NULL DEFAULT "",
+    `guide`      TEXT NOT NULL DEFAULT "",
     `updated_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
