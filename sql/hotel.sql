85ab821588ca912a01178d0768fefb2964219cc6
Ice White <yashiro1899@gmail.com>
2014-04-15 11:54:51 +0800

    1397534091

--- sql/hotel.sql
+++ sql/hotel.sql
@@ -1,6 +1,5 @@
 CREATE TABLE IF NOT EXISTS `think_hotel` (
     `hotelid`    INT(11) UNSIGNED NOT NULL,
-    `hotelcd`    VARCHAR(32) NOT NULL,
     `namechn`    VARCHAR(64) NOT NULL,
     `nameeng`    VARCHAR(64) NOT NULL,
     `country`    INT(11) UNSIGNED NOT NULL,
@@ -8,7 +7,6 @@ CREATE TABLE IF NOT EXISTS `think_hotel` (
     `city`       INT(11) UNSIGNED NOT NULL,
     `website`    VARCHAR(128) NOT NULL,
     `original`   TEXT NOT NULL,
-    `taobao_hid` INT(11) UNSIGNED NOT NULL DEFAULT 0,
     `updated_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     PRIMARY KEY (`hotelid`),
     KEY country (`country`),
--- sql/room.sql
+++ sql/room.sql
@@ -4,10 +4,8 @@ CREATE TABLE IF NOT EXISTS `think_room` (
     `namechn`          VARCHAR(64) NOT NULL,
     `status`           INT(11) UNSIGNED NOT NULL DEFAULT 0,
     `original`         TEXT NOT NULL,
-    `taobao_rid`       INT(11) UNSIGNED NOT NULL DEFAULT 0,
     `no_price_expires` BIGINT(13) UNSIGNED NOT NULL DEFAULT 0,
     `updated_at`       TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     PRIMARY KEY (`roomtypeid`),
-    KEY taobao_rid (`taobao_rid`),
     CONSTRAINT `hotelid` FOREIGN KEY (`hotelid`) REFERENCES `think_hotel` (`hotelid`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
