231b0bdeed5d26b382bbf0eac56335b73d801104
Ice White <yashiro1899@gmail.com>
2014-04-22 10:53:15 +0800

    1398135195

---
+++ www/redirect.js
@@ -0,0 +1,8 @@
+var http = require("http");
+
+http.createServer(function(req, res) {
+    res.writeHead(301, {
+        "location": "http://121.196.142.144"
+    });
+    res.end();
+}).listen(8360, "127.0.0.1");
