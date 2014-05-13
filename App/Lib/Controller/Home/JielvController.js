819354971d5b03e3eacf53c7f1430e93d5c0ccdf
Ice White <yashiro1899@gmail.com>
2014-05-13 19:31:59 +0800

    1399980719

--- App/Lib/Controller/Home/JielvController.js
+++ App/Lib/Controller/Home/JielvController.js
@@ -86,7 +86,7 @@ module.exports = Controller(function() {
                 if (roomtypeids.length === 0) return null;
 
                 var time = dateformat(new Date(), "[yyyy-mm-dd HH:MM:ss]");
-                console.log(time, "jielv.callback", roomtypeids.length, "roomtypeids");
+                console.log(time, "jielv.callback", roomtypeids.sort().join(","), "roomtypeids");
 
                 var users = {};
                 var model = D("Goods").where("roomtypeid in (" + roomtypeids.join(",") + ") and status = 4");
