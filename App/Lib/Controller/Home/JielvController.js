c8e1604a9afa5d29f3840bb227329c1b83859307
Ice White <yashiro1899@gmail.com>
2014-05-30 02:37:58 +0800

    1401388678

--- App/Lib/Controller/Home/JielvController.js
+++ App/Lib/Controller/Home/JielvController.js
@@ -603,6 +603,7 @@ module.exports = Controller(function() {
                             });
                         });
                     }
+                    if (parameters.length === 0) return getDefer().Promise();
 
                     var pieces = [];
                     var block = 800;
