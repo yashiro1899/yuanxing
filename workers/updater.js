4d4b5e357bf2648c84190a78a7a3927243fa94bc
Ice White <yashiro1899@gmail.com>
2014-06-10 18:21:50 +0800

    1402395710

--- workers/updater.js
+++ workers/updater.js
@@ -1,6 +1,6 @@
 process.on('message', function(data) {
 setTimeout(function() {
-process.exit(4);
+    process.exit(4);
 }, 30 * 60 * 1000);
 var roomtypeids = data.roomtypeids;
 var users = data.users;
@@ -106,7 +106,7 @@ for (; i < length; i += 1) {
 
 var goods = {};
 deferred.promise.then(function(result) { // hotelpriceall
-console.log(dateformat(new Date(), "[yyyy-mm-dd HH:MM:ss]"), "---------->", roomtypeids.length, Object.keys(quotas)["length"]);
+    console.log(dateformat(new Date(), "[yyyy-mm-dd HH:MM:ss]"), "---------->", roomtypeids.length, Object.keys(quotas)["length"]);
     var dfd = getDefer();
     var querystring = "select gid,userid,roomtypeid,ratetype,ptype,profit from think_goods where roomtypeid in (" + roomtypeids.join(",") + ") and userid in (" + Object.keys(users).join(",") + ") and status = 4";
     roomtypeids = null;
