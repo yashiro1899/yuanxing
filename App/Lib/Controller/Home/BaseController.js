2aaf290520dc981329b5ffa4162eca72738e4b5f
Ice White <yashiro1899@gmail.com>
2014-06-14 17:41:08 +0800

    1402738868

--- App/Lib/Controller/Home/BaseController.js
+++ App/Lib/Controller/Home/BaseController.js
@@ -12,7 +12,7 @@ module.exports = Controller(function() {
             this.super_("init", http);
             this.assign("title", this.title || "");
             this.assign("navType", this.navType || "");
-            this.assign("staticversion", "1402731505");
+            this.assign("staticversion", "1402738050");
 
             var that = this;
             return oauth.getUserInfo(this.http.req, this.http.res).then(function(u) {
